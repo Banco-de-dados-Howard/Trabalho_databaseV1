e8c79a3c9d6669e20028421cfed730061a5aa766
MatheusPas <mathimor127@hotmail.com>
2025-10-20 13:15:00 -0300

    Criado o executável

--- Ifode/obj/Release/net8.0/Ifode.AssemblyInfo.cs
+++ Ifode/obj/Release/net8.0/Ifode.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ifode")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc0942230a401f1aa8d59798776ec1a6e95ac32f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ifode")]
