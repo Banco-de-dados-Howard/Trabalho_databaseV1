a8e15880963041b4818b181761219cd7169961b4
MatheusPas <mathimor127@hotmail.com>
2025-10-21 17:02:21 -0300

    modified:   Ifode/obj/Debug/net8.0/Ifode.AssemblyInfo.cs 	modified:   Ifode/obj/Debug/net8.0/Ifode.AssemblyInfoInputs.cache

--- Ifode/obj/Debug/net8.0/Ifode.AssemblyInfo.cs
+++ Ifode/obj/Debug/net8.0/Ifode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ifode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2775b13d294b314e576bfd6f2e8059c859dab425")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8d2ccc2dd3224c6137aa2a5dd867fac7055067")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ifode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ifode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
